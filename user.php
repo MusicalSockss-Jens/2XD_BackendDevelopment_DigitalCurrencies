3ad35dddbe8b28d6379b23c9b9bbbc3549eab45b
MusicalSockss-Jens <r1053627@student.thomasmore.be>
2026-08-14 14:59:03 +0200

    refactor: simplified code

--- user.php
+++ user.php
@@ -1,11 +1,21 @@
 <?php
 
-class User {
-    private $pdo;
+  class User {
     private $email;
     private $password;
 
-    public function __construct($pdo) {
-        $this->pdo = $pdo;
+    public function setEmail($email) {
+        $email = trim($email);
+        if (!str_ends_with($email, '@student.thomasmore.be')) {
+            throw new Exception("Het e-mailadres moet eindigen op @student.thomasmore.be");
+        }
+        $this->email = $email;
+    }
+
+    public function setPassword($password) {
+        if (strlen($password) < 5) {
+            throw new Exception("Het wachtwoord moet minstens 5 tekens lang zijn.");
+        }
+        $this->password = password_hash($password, PASSWORD_BCRYPT);
     }
 }
